c926e2290c3b8bd7812854401cf84acadb2ecd33
Lavaria <pedrinho_gui@live.com>
2024-10-14 23:28:52 -0300

    atua do jogo

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function selectRandomItems(numItems) {
     const items = document.querySelectorAll('#itemList li');
     const selectedItems = [];
@@ -12,7 +11,7 @@ function selectRandomItems(numItems) {
     }
 
     // Montando o HTML de saída
-    let resultHTML = '<h2>Itens Selecionados:</h2><ul>';
+    let resultHTML = '<h2>Sorte das cartas:</h2><ul>';
     selectedItems.forEach(item => {
         const itemName = item.querySelector('.item-text').textContent.trim(); // Nome do item
         const description = item.querySelector('.descricao').textContent.trim(); // Descrição
@@ -23,3 +22,17 @@ function selectRandomItems(numItems) {
     // Atualizando o conteúdo do resultado
     document.getElementById('result').innerHTML = resultHTML;
 }
+
+// jQuery para mostrar a descrição com efeito de girar
+$(document).ready(function() {
+    $('#itemList li').hover(
+        function() {
+            $(this).find('.descricao').addClass('show-descricao');
+            $(this).find('img').css('opacity', '0.2'); // Reduz a opacidade da imagem
+        },
+        function() {
+            $(this).find('.descricao').removeClass('show-descricao');
+            $(this).find('img').css('opacity', '1'); // Restaura a opacidade da imagem
+        }
+    );
+});
